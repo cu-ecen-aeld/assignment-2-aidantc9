3918812c1c05c3e81c1676398b25cd2148130ae0
aidantc9 <aidantc9@gmail.com>
2024-01-07 16:29:22 -0500

    assignment2done

---
+++ finder-app/writer.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <syslog.h>
+int main (int argc, char *argv[] ){
+    openlog(NULL,0,LOG_USER);
+    if (argc != 3){
+        syslog(LOG_ERR,"Invalid number of arguments: %d",argc);
+    }
+    else{
+        syslog(LOG_DEBUG,"Writing %s to %s",argv[2],argv[1]);
+        FILE *file;
+        file = fopen(argv[1],"w+");
+        fprintf(file,argv[2]);
+        fclose(file);
+
+    }
+}
